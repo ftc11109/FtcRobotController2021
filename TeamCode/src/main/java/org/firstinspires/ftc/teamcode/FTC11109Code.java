5c03be2c974447eb8f89454b5f96df0264c55b47
FTC11109 <ftc11109@gmail.com>
2022-10-22 17:09:05 -0700

    chaya/emry: added last years auto drive and turn code changed opmode type from iterative to linear

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC11109Code.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC11109Code.java
@@ -32,12 +32,38 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import android.graphics.Color;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import java.util.List;
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaCurrentGame;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.Tfod;
 
 
 /**
@@ -55,8 +81,9 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
  */
 
 // @Disabled
-public class FTC11109Code extends OpMode
-{
+public class FTC11109Code extends LinearOpMode {
+
+
     // Declare OpMode members.
     private final ElapsedTime runtime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
     private DcMotor driveLF = null;
@@ -75,11 +102,28 @@ public class FTC11109Code extends OpMode
     private BNO055IMU imu;
     double initialAngle;
 
+    String startColor;
+    String startAorJ;
+
+    @Override
+    public void runOpMode() {
+        myInit();
+        while (!opModeIsActive()) {
+            myInitLoop();
+        }
+        myStart();
+        while (opModeIsActive()) {
+            myLoop();
+            loop();
+        }
+        myStop();
+    }
+
+
     /*
      * Code to run ONCE when the driver hits INIT
      */
-    @Override
-    public void init() {
+    public void myInit() {
         teleop = true; //move to the extended opmodes when we figure out how
         fieldOrientated = true;
         parabolicDriving = teleop;
@@ -131,29 +175,41 @@ public class FTC11109Code extends OpMode
         initialAngle = angles.firstAngle;
 
 
-
     }
 
     /*
      * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
      */
-    @Override
-    public void init_loop() {
+    public void myInitLoop() {
+
+        if (gamepad1.x) {
+            startColor = "blue";
+        } else if (gamepad1.b) {
+            startColor = "red";
+        }
+
+        if (gamepad1.y) {
+            startAorJ = "judges";
+        } else if (gamepad1.a) {
+            startAorJ = "audience";
+        }
+
+        telemetry.addData("startColor", startColor);
+        telemetry.addData("startAudienceOrWarehouse", startAorJ);
+
     }
 
     /*
      * Code to run ONCE when the driver hits PLAY
      */
-    @Override
-    public void start() {
+    public void myStart() {
         runtime.reset();
     }
 
     /*
      * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
      */
-    @Override
-    public void loop() {
+    public void myLoop() {
         // Get absolute orientation
         angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
 
@@ -175,8 +231,7 @@ public class FTC11109Code extends OpMode
     /*
      * Code to run ONCE after the driver hits STOP
      */
-    @Override
-    public void stop() {
+    public void myStop() {
         driveLF.setPower(0);
         driveLB.setPower(0);
         driveRF.setPower(0);
@@ -184,13 +239,10 @@ public class FTC11109Code extends OpMode
     }
 
 
-
-
-
+    //teleop drive code
     private double adjustForVoltage(double power) {
         return power;
     }
-
 
     private void driveMotors(double fwd, double strafe, double rotate) {
         double tempForward;
@@ -247,9 +299,6 @@ public class FTC11109Code extends OpMode
         }
     }
 
-
-
-
     private void gamepadDriveMotors() {
         double fwdSign;
         double strafeSign;
@@ -298,7 +347,6 @@ public class FTC11109Code extends OpMode
         }
     }
 
-
     private double parabolicTransform(double stick) {
         if (stick < 0) {
             stick = -Math.pow(stick, 2);
@@ -307,11 +355,6 @@ public class FTC11109Code extends OpMode
         }
         return stick;
     }
-
-
-
-
-
 
     private double teleopTurnToAngle(double targetAngle, double currentAngle, double powerIn, double powerIn2) {
         double angleDifference = targetAngle - currentAngle;
@@ -344,6 +387,182 @@ public class FTC11109Code extends OpMode
         }
         return rotateOut;
     }
+
+
+    //autonomous drive code
+    private void runToPositionInit(int targetPosition, double power) {
+        driveLF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveLB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveRF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveRB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveLF.setPower(power);
+        driveLB.setPower(power);
+        driveRF.setPower(power);
+        driveRB.setPower(power);
+        driveLF.setTargetPosition(targetPosition);
+        driveLB.setTargetPosition(targetPosition);
+        driveRF.setTargetPosition(targetPosition);
+        driveRB.setTargetPosition(targetPosition);
+        driveLF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        driveLB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        driveRF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        driveRB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    private void driveTelemetryRunToPosition() {
+        // set how close to the target position the robot stops at
+        // set last position to current position
+        // current position to the current position
+        double currentPositionLF = driveLF.getCurrentPosition();
+        double currentPositionLB = driveLB.getCurrentPosition();
+        double currentPositionRF = driveRF.getCurrentPosition();
+        double currentPositionRB = driveRB.getCurrentPosition();
+        telemetry.addData("currentPositionLF", currentPositionLF);
+        telemetry.addData("currentPositionLB", currentPositionLB);
+        telemetry.addData("currentPositionRF", currentPositionRF);
+        telemetry.addData("currentPositionRB", currentPositionRB);
+        telemetry.addData("isBusyLF", driveLF.isBusy());
+        telemetry.addData("isBusyLB", driveLB.isBusy());
+        telemetry.addData("isBusyRF", driveRF.isBusy());
+        telemetry.addData("isBusyRB", driveRB.isBusy());
+
+    }
+
+    private void runToPosition(double targetInches, double power, int sleepTime, double tolerance) {
+        int targetPosition = (int) (targetInches * 31.25);
+        tolerance = tolerance * 31.25;
+        runToPositionInit(targetPosition, power);
+        while (opModeIsActive()) {
+            if (telemetryEnabled) {
+                driveTelemetryRunToPosition();
+            }
+
+            if (Math.abs(targetPosition - driveLF.getCurrentPosition()) < tolerance && Math.abs(targetPosition - driveRF.getCurrentPosition()) < tolerance && Math.abs(targetPosition - driveLB.getCurrentPosition()) < tolerance && Math.abs(targetPosition - driveRB.getCurrentPosition()) < tolerance) {
+                break;
+            }
+            if (driveLF.isBusy() || driveLB.isBusy() || driveRF.isBusy() || driveRB.isBusy()) {
+                continue;
+            }
+            break;
+        }
+        sleep(sleepTime);
+    }
+
+    private void initRunToPositionLeftRight(int targetPositionL, int targetPositionR, double powerL, double powerR, int sleepTime) {
+        driveLF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveLB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveRF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveRB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveLF.setPower(powerL);
+        driveRF.setPower(powerR);
+        driveLB.setPower(powerL);
+        driveRB.setPower(powerR);
+        driveLF.setTargetPosition((int) targetPositionL);
+        driveRF.setTargetPosition((int) targetPositionR);
+        driveLB.setTargetPosition((int) targetPositionL);
+        driveRB.setTargetPosition((int) targetPositionR);
+        driveLF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        driveRF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        driveLB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        driveRB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    }
+
+    private void runToPositionLeftRight(double targetInchesL, double targetInchesR, double powerL, double powerR, int sleepTime, double tolerance) {
+        int targetPositionL = (int) (targetInchesL * 31.25);
+        int targetPositionR = (int) (targetInchesR * 31.25);
+        tolerance = tolerance * 31.25;
+        ((DcMotorEx) driveLF).setTargetPositionTolerance((int) tolerance);
+        ((DcMotorEx) driveRF).setTargetPositionTolerance((int) tolerance);
+        ((DcMotorEx) driveLB).setTargetPositionTolerance((int) tolerance);
+        ((DcMotorEx) driveRB).setTargetPositionTolerance((int) tolerance);
+        initRunToPositionLeftRight((int) targetPositionL, (int) targetPositionR, powerL, powerR, sleepTime);
+        while (opModeIsActive()) {
+            
+            if (driveLF.isBusy() || driveLB.isBusy() || driveRF.isBusy() || driveRB.isBusy()) {
+                continue;
+            }
+            break;
+        }
+        sleep(sleepTime);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void turnToAngle(int targetAngle, double turnPower, double turnPower2, int sleepTime) {
+        turn(targetAngle, turnPower, turnPower2, 2, 4, 4);
+    }
+
+
+    private void turn(int targetAngle, double powerIn, double powerIn2, int tolerance, int targetReachedCountThreshold, int failSafeCountThreshold) {
+        double rotate;
+        int failSafeCount;
+        int targetReachedCount;
+
+        failSafeCount = 0;
+        targetReachedCount = 0;
+        while (opModeIsActive()) {
+            angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            double currentAngle = angles.firstAngle;
+            double angleDifference = targetAngle - currentAngle;
+            if (angleDifference > 180) {
+                angleDifference += -360;
+            }
+            if (angleDifference < -180) {
+                angleDifference += 360;
+            }
+            if (telemetryEnabled) {
+                telemetry.addData("angleDifference", angleDifference);
+                telemetry.addData("currentAngle", currentAngle);
+            }
+
+            double power;
+
+            if (Math.abs(angleDifference) < 10) {
+                power = powerIn2;
+            } else if (Math.abs(angleDifference) < 40) {
+                power = (Math.abs(angleDifference) / 40) * (powerIn - powerIn2) + powerIn2;
+            } else {
+                power = powerIn;
+            }
+            if (angleDifference < -tolerance) {
+                targetReachedCount = 0;
+                rotate = power;
+            } else if (angleDifference > tolerance) {
+                targetReachedCount = 0;
+                rotate = -1 * power;
+            } else {
+                targetReachedCount += 1;
+                failSafeCount += 1;
+                rotate = 0;
+                driveMotors(0, 0, 0);
+                if (targetReachedCount >= targetReachedCountThreshold || failSafeCount >= failSafeCountThreshold) {
+                    break;
+                }
+            }
+            driveMotors(0, 0, (float) rotate);
+            telemetry.update();
+        }
+        driveMotors(0, 0, 0);
+        telemetry.update();
+    }
+
+
+
+
+
+
 
 
 
